fc916f12cac183fd566fec02e82494cfaf6df147
LadyImpatient <aliciapuuwai@gmail.com>
2024-05-09 21:15:01 -0500

    i don't even remember what I fixed. I need to commit more often lmao

--- scripts/hamburgerMenu.js
+++ scripts/hamburgerMenu.js
@@ -2,5 +2,10 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('nav ul');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('show');
+  menu.classList.toggle('show');
+});
+
+// Hide the menu when a link is clicked
+menu.addEventListener('click', () => {
+  menu.classList.remove('show');
 });
