018c69b8d46ce5cab5a1820aa3665f54c577fbb5
LadyImpatient <aliciapuuwai@gmail.com>
2024-05-13 14:38:56 -0500

    fixing js linking

--- chamber/scripts/visitInfo.js
+++ chamber/scripts/visitInfo.js
@@ -1,13 +1,24 @@
-const lastVisit = localStorage.getItem('lastVisit');
-if (!lastVisit) {
-    document.write("<p>Welcome! Let us know if you have any questions.</p>");
-} else {
-    const currentDate = Date.now();
-    const daysSinceVisit = Math.floor((currentDate - lastVisit) / (1000 * 60 * 60 * 24));
-    if (daysSinceVisit < 1) {
-        document.write("<p>Back so soon! Awesome!</p>");
-    } else {
-        document.write(`<p>You last visited ${daysSinceVisit} ${daysSinceVisit === 1 ? 'day' : 'days'} ago.</p>`);
-    }
+const msToDays = 84600000;
+// today's date
+const theDateToday = new Date();
+
+// initialize display elements
+const todayElement = document.querySelector("#today");
+const christmasElement = document.querySelector("#christmas");
+const christmasDateElement = document.querySelector("#christmasDate");
+const daysElement = document.querySelector("#daysleft");
+
+// processing
+const today = Date.now();
+const christmasDate = new Date(Date.UTC(theDateToday.getFullYear(), 11, 25));
+// check if is the waing days of December, if so ... change to next year.
+if (theDateToday.getMonth() == 11 && theDateToday.getDate() > 25) {
+	christmasDate.setFullYear(christmasDate.getFullYear() + 1);
 }
-localStorage.setItem('lastVisit', Date.now());
+// find difference between epoch times in ms and convert to days
+let daysleft = (christmasDate.getTime() - Date.now()) / msToDays;
+
+todayElement.textContent = today;
+christmasElement.textContent = christmasDate.getTime();
+christmasDateElement.textContent = christmasDate;
+daysElement.textContent = `${daysleft.toFixed(0)} days`;
