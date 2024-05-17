4e2a3c9ae853c119f083d949c1d00277ba318b42
LadyImpatient <aliciapuuwai@gmail.com>
2024-05-17 15:40:22 -0500

    trying to fix the header issues with no avail

--- chamber/scripts/hamburgerMenu.js
+++ chamber/scripts/hamburgerMenu.js
@@ -1,19 +1,17 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('nav ul');
-
-menuBtn.addEventListener('click', () => {
-  menu.classList.toggle('show');
-});
-
-menu.addEventListener('click', () => {
-  menu.classList.remove('show');
-});
-
 document.addEventListener('DOMContentLoaded', function () {
   const menuToggle = document.querySelector('#menu-toggle');
   const nav = document.querySelector('nav');
 
-  menuToggle.addEventListener('click', function () {
+  if (menuToggle && nav) {
+    menuToggle.addEventListener('click', function () {
       nav.classList.toggle('active');
+    });
+  }
+
+  nav.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      nav.classList.remove('active');
+      menuToggle.checked = false;
+    }
   });
 });
